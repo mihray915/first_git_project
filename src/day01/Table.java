fa98d6ac27e11d542fd82b81b272b403d8901fc3
mihra <mihray0126@gmail.com>
2025-08-31 11:44:46 -0400

    HelloWorld class , Added Table class with multiplicationTable, and method

---
+++ src/day01/Table.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class Table {
+    public static void main(String[] args) {
+        
+    }
+    public static void multiplication(int num){
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println(num + " * "+i +" = "+ (num * i) );
+        }
+    }
+}
---
+++ src/day01/main.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+
+        Table.multiplication(5);
+        Table.multiplication(3);
+    }
+}
